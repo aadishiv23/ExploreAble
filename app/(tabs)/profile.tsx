514bb20f232f3aa4403061c3b947742d898a8b5d
sttcha31 <sttcha31@gmail.com>
2025-01-29 20:13:35 -0500

    profilesetupprogress

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -5,12 +5,13 @@ import { StatusBar } from 'expo-status-bar'
 import { supabase } from '@/utils/supabase'
 import { Session } from '@supabase/supabase-js'
 import { createClient } from '@supabase/supabase-js'
-
+import { useRouter } from 'expo-router'
 import { MEDITATION_DATA } from '@/constants/MeditationData';
 import MEDITATION_IMAGES from '@/constants/meditation-images';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Profile({ session }: { session: Session }) {
+    const router = useRouter()
     const [loading, setLoading] = useState(true)
     //TODO change to empty quote when we get authentication working
     const [name, setName] = useState('Full Name')
@@ -56,55 +57,33 @@ export default function Profile({ session }: { session: Session }) {
 
   return (
     <View className='flex-1'>
-      <AppGradient colors={["#161b2e", "#0a4d4a", "#766e67"]}>
+      <AppGradient colors={["FFFFFF", "FFFFFF"]}>
         <View className='mb-6 justify-center'>
-            <Text className='text-gray-200 mb-3 font-bold text-4xl text-center'>
+            <Text className='text-black mb-3 font-bold text-4xl text-center'>
                 {name}
             </Text>
             
             <Pressable
                 className='h-32 w-32 rounded-full bg-white mx-auto mb-4'
             ></Pressable>
-            <Text className='text-indigo-100 text-xl font-medium text-center'>
+            <Text className='text-black text-xl font-medium text-center'>
                 @user-name
             </Text>
-            <Text className='text-gray-200 mb-3 font-bold text-xl text-center'>
+            <Text className='text-black mb-3 font-bold text-xl text-center'>
                 Age: {age}
             </Text>
-            <Text className='text-gray-200 mb-3 font-bold text-xl text-center'>
+            <Text className='text-black mb-3 font-bold text-xl text-center'>
                 Disability: {disablity}
             </Text>
         </View>
 
         <View>
-            <FlatList
-                data={MEDITATION_DATA}
-                className='mb-20'
-                keyExtractor={( item ) => item.id.toString()}
-                showsVerticalScrollIndicator={false}
-                renderItem={({ item }) => (
-                    <Pressable
-                    onPress={() => console.log("press")}
-                    className="h-48 my-3 rounded-md overflow-hidden"
-                    >
-                        <ImageBackground
-                            source={MEDITATION_IMAGES[item.id - 1]}
-                            resizeMode='cover'
-                            className='flex-1 rounded-lg justify-center'
-                        >
-                            <LinearGradient
-                                colors={[
-                                    "transparent",
-                                    "rgba(0, 0, 0, 0.8)"
-                                ]}
-                                className='flex-1 justify-center items-center'
-                            > 
-                                <Text className='text-gray-100 text-3xl font-bold text-center'>{item.title}</Text>
-                            </LinearGradient>
-                        </ImageBackground>
-                    </Pressable>
-                )}
-            />
+            <Pressable
+            onPress={() => router.push("/editprofile")}
+            className="h-48 my-3 rounded-md overflow-hidden"
+            >
+                <Text className='text-gray-100 text-3xl font-bold text-center'>Edit Profile</Text>
+            </Pressable>
         </View>
       </AppGradient>
 
